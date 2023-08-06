1abe8452cbeb9d8d2bce21b40de5dada1dbf7b0a
Luca G <luca.p.giannotti@gmail.com>
2023-08-05 22:26:05 -0500

    📢 I accidently used Y instead of Z

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function calculateAngle(x1, y1, x2, y2) {
+function calculateAngle(x1, z1, x2, z2) {
     const deltaX = x2 - x1;
-    const deltaY = y2 - y1;
-    const angleRad = Math.atan2(deltaY, deltaX);
-    const angleDeg = angleRad * (180 / Math.PI);
+    const deltaZ = z2 - z1;
+    
+    // Calculate angle using Minecraft's coordinate system (0 degrees facing south, positive clockwise)
+    const angleRad = Math.atan2(deltaZ, deltaX);
+    let angleDeg = angleRad * (180 / Math.PI);
+    angleDeg -= 90;
+    if (angleDeg < 0) {
+        angleDeg += 360;
+    }
+
     return angleDeg;
 }
 
-function calculateDistance(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+function calculateDistance(x1, z1, x2, z2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(z2 - z1, 2));
 }
 
 function calculateTravelTime() {
     const x1 = parseFloat(document.getElementById('x1').value);
-    const y1 = parseFloat(document.getElementById('y1').value);
+    const z1 = parseFloat(document.getElementById('z1').value);
     const x2 = parseFloat(document.getElementById('x2').value);
-    const y2 = parseFloat(document.getElementById('y2').value);
+    const z2 = parseFloat(document.getElementById('z2').value);
 
     const runningSpeed = 5.612;  // units per second
     const jumpingSpeed = 7.127;  // units per second
 
-    const angleDeg = calculateAngle(x1, y1, x2, y2);
-    const distance = calculateDistance(x1, y1, x2, y2);
+    const angleDeg = calculateAngle(x1, z1, x2, z2);
+    const distance = calculateDistance(x1, z1, x2, z2);
     const travelTimeRunning = distance / runningSpeed / 60;  // Convert to minutes
     const travelTimeJumping = distance / jumpingSpeed / 60;  // Convert to minutes
 
